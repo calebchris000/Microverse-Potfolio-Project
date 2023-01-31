8ad2ee35418578681dbfdb111911043888427a64
calebchris000 <calebchris000@gmail.com>
2023-01-31 11:01:23 +0100

    Remove newline at the end of line, fix bug causing clicking on nav link toggling the menu overlay

--- app.js
+++ app.js
@@ -4,9 +4,4 @@ let nav = document.querySelector('.menulist');
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('active');
     nav.classList.toggle('active');
-})
-
-nav.addEventListener('click', function(){
-    nav.classList.toggle('active');
-    hamburger.classList.toggle('active');
-})
+});
