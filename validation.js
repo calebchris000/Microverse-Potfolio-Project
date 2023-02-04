8e2b4baf4152eb973a53276aafd3d14cdf500b8f
calebchris000 <calebchris000@gmail.com>
2023-02-04 18:20:33 +0100

    Add descriptive comments, add more visual info on the status of the email address inputed

--- validation.js
+++ validation.js
@@ -1,65 +1,94 @@
 // Validation
-let submitForm = document.querySelector("#submitForm");
-let email = document.querySelector("#mail");
-let form = document.querySelector("form");
+let submitForm = document.querySelector('#submitForm');
+let email = document.querySelector('#mail');
+let form = document.querySelector('form');
 
+// The regex for the email address
 let regex = /^[a-z0-9_.%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/u;
+
+// The regex for detecting capitalization
 let checkCaps = /[A-Z]+/u;
+
+// Number set for the timeouts.
 let timeout = 3000;
+
+// The style to be set if an error occured on the form input(For email for now)
+let borderError = {
+    'border': '3px solid red',
+    'transition': 'all 0.5s ease-in-out'
+}
+
+// The style for the error message
 let errorStyle = {
-  backgroundColor: "red",
-  color: "#fff",
-  opacity: "0",
-  transition: "all 0.5s ease",
-  userSelect: "none",
-  width: "fit-content",
+    'backgroundColor': 'orange',
+    'color': '#000',
+    'opacity': '0',
+    'transition': 'all 0.5s ease',
+    'userSelect': 'none',
+    'width': 'fit-content'
 };
 
-let errormsg = document.createElement("p");
+
+// The error message creation
+let errormsg = document.createElement('p');
 Object.assign(errormsg.style, errorStyle);
-errormsg.textContent = "hello";
+errormsg.textContent = 'hello';
 form.appendChild(errormsg);
 
-submitForm.addEventListener("click", (event) => {
-  let EmailValidation = regex.test(email.value);
-  if (checkCaps.test(email.value)) {
+// Function that writes out the error message
+let reportError = (report) => {
     setTimeout(function () {
-      errormsg.style.opacity = "0";
+        errormsg.style.opacity = '0';
+        email.style.borderColor = 'rgba(0,0,0,0)'
     }, timeout);
-    errormsg.style.opacity = "1";
-    errormsg.innerHTML = "Error! capital letters are disallowed";
+    errormsg.style.opacity = '1';
+    errormsg.innerHTML = report;
     event.preventDefault();
-  } else if (!EmailValidation) {
-    setTimeout(function () {
-      errormsg.style.opacity = "0";
-    }, timeout);
-    errormsg.style.opacity = "1";
-    errormsg.innerHTML = "Error! Email Address not valid";
-    event.preventDefault();
-  }
+}
+
+// Testing the email validation
+let EmailValidation = regex.test(email.value);
+
+// Listens to the submit button, checks if the email address is filled correctly
+submitForm.addEventListener('click', () => {
+
+    if (email.value === '') {
+        Object.assign(email.style, borderError);
+        let report = 'Please type your email address';
+        reportError(report)
+    } else if (checkCaps.test(email.value)) {
+        Object.assign(email.style, borderError);
+        let report = 'Error! capital letters are disallowed'
+        reportError(report)
+    } else if (!EmailValidation) {
+        Object.assign(email.style, borderError);
+        let report = 'Error! Email Address not valid'
+        reportError(report);
+    }
 });
 
 // Local Storage and Session Storage code
-let userName = document.querySelector("#name");
-let content = document.querySelector("#msg");
+let userName = document.querySelector('#name');
+let content = document.querySelector('#msg');
 
 // Get input data on the input field
-form.addEventListener("keyup", () => {
-  let data = {};
-  data.name = userName.value;
-  data.email = email.value;
-  data.content = content.value;
+let data = {};
+form.addEventListener('keyup', () => {
+    data.name = userName.value;
+    data.email = email.value;
+    data.content = content.value;
 
-  let stringD = JSON.stringify(data);
-  localStorage.setItem("data", stringD);
+    let stringD = JSON.stringify(data);
+    localStorage.setItem('data', stringD);
 });
 
-// Sets the data back to the input
+// Function that sets the data back to the input
 let setItems = () => {
-  let setData = JSON.parse(localStorage.getItem("data"));
-  userName.value = setData.name;
-  email.value = setData.email;
-  content.value = setData.content;
+    let setData = JSON.parse(localStorage.getItem('data'));
+    userName.value = setData.name;
+    email.value = setData.email;
+    content.value = setData.content;
 };
 
+// Sets the saved local data back to the input field on refresh or restart
 window.onload = setItems;
